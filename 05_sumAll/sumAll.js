6bb57e394bde709e3c914eff8bc42388bb016f0a
Rayyan <124036859+Gigadev99@users.noreply.github.com>
2023-03-31 11:15:43 +0800

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,32 @@
 const sumAll = function(a, b) {
-    
-    return a
-
+    let num = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR"
+    }
+    // this loop will sum from a to b, or b to a if a is larger.
+    if (b > a) {
+        for (let i = a; i <= b; i++) {
+            num += i
+        }
+    }
+    else    {
+        for (let i = b; i <= a; i++) {
+            num += i
+        }
+    }
+    return num;
 };
+//Simpler method using formula of consecutive sum
+const sumAll2 = function(a,b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR"
+    }
+    
+    return (a + b)/2 * (Math.abs(b - a) + 1)
+    
+    
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll2;
+
