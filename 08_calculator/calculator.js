1dbf2108c714d1852f408ed9d3cb21d4b08c1994
Rayyan <124036859+Gigadev99@users.noreply.github.com>
2023-04-01 07:40:18 +0800

    done till 8 : )

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return Math.abs(x - y)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let l = arr.length
+  let sum = 0;
+	for (let i = 0; i < l; i++) {
+    sum += arr[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let l = arr.length
+  let product = 1;
+	for (let i = 0; i < l; i++) {
+    product *= arr[i]
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(X) {
+ // Recursion!
+	if (X == 0) {
+    return 1;
+  }
+  else {
+    return  X * factorial(X - 1);
+  }
 };
 
 // Do not edit below this line
