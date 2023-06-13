fb9fb8dc0a2bd75d1f8761d4928e972bf4c9601e
Gigadev99 <124036859+Gigadev99@users.noreply.github.com>
2023-06-13 17:53:18 +0800

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    total = 1;
+    let New;
+    let Old = 1;
+    if (n < 0) return "OOPS"
+    for (let i = 1; i < n - 1; i++) {
+        New = total + Old
+        Old = total
+        total = New
+    }
+    return total
 };
 
 // Do not edit below this line
