4c4a32c43cf7ca32dc355dfa4e63c173cdc09d9a
Gigadev99 <124036859+Gigadev99@users.noreply.github.com>
2023-06-13 14:50:32 +0800

    done palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    phrase = phrase.toLowerCase()
+    let a = phrase.replace(/[^a-z]/g, "")
+    let b = "";
+    for (let i = a.length - 1; i >= 0; i--) {
+        b += a[i]
+    }
+    return a == b
 };
 
 // Do not edit below this line
