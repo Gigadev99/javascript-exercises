d7e1f7fd93a8a7624e597157776aa465cf037e4f
Gigadev99 <124036859+Gigadev99@users.noreply.github.com>
2023-06-14 09:21:07 +0800

    done 12_findTheOldest done everythin' done life

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
 
+    const oldest = persons.sort((a, b) => {
+        var adeath = a.yearOfDeath
+        var bdeath = b.yearOfDeath
+        if (a.yearOfDeath == undefined) {
+            adeath = new Date().getFullYear()
+        }
+        if (b.yearOfDeath == undefined) {
+            bdeath = new Date().getFullYear()
+        }
+        return bdeath - b.yearOfBirth - adeath + a.yearOfBirth
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
